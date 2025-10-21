03e1cedefb5d1b0c3c7f0632f715c695d11a94b8
nishita2094 <nishita.grover@edu.sait.ca>
2025-10-21 10:32:44 -0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,26 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { NotificationProvider } from "../contexts/NotificationContext";
 import '../globals.js';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="signin" options={{ headerShown: false }} />
-      <Stack.Screen name="signin_followup" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* Define the capture screen as a full-screen modal */}
-      <Stack.Screen 
-        name="capture" 
-        options={{ 
-          headerShown: false, 
-          presentation: 'fullScreenModal' 
-        }} 
-      />
-    </Stack>
+    <NotificationProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="signin" options={{ headerShown: false }} />
+        <Stack.Screen name="signin_followup" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="notifications" options={{ headerShown: false }} />
+        {/* Define the capture screen as a full-screen modal */}
+        <Stack.Screen 
+          name="capture" 
+          options={{ 
+            headerShown: false, 
+            presentation: 'fullScreenModal' 
+          }} 
+        />
+      </Stack>
+    </NotificationProvider>
   );
 }
